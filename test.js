31e6e3f7fd59a206a33f7e4f4b2ee4edb006fcda
Anna Czapiewska <148961097+anna-czapiewska-wttech@users.noreply.github.com>
2024-08-14 07:23:23 +0000

    mm

--- test.js
+++
@@ -1,9 +0,0 @@
-document.dispatchEvent(new CustomEvent('saveArticleLogiIn'));
-
-var handleSaveArticleLogin = function() {
-    console.log('llll');
-};
-
-var initSaveArticleOverlay = function() {
-    document.addEventListener('saveArticleLogiIn', handleSaveArticleLogin);
-};
